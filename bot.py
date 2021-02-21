fa82845483c37921682880b3ba16af86363d2cf2
Skylake-dev <77413867+Skylake-dev@users.noreply.github.com>
2021-02-22 00:03:29 +0100

    aggiunto messaggio di benvenuto per i nuovi membri

--- bot.py
+++ bot.py
@@ -9,7 +9,9 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 MAIN_CHANNEL = int(os.getenv('MAIN_CHANNEL'))
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -31,5 +33,19 @@ async def on_message(message):
     if message.content == '69' or message.content == '420':
         response = 'nice'
         await message.channel.send(response)
+
+@client.event
+async def on_member_join(member):
+    print('nuovo membro')
+    greetings = """Benvenuto/a sul server discord AFL! \n
+    Prima di iniziare a partecipare nel server presentati nel canale richiesta di afl indicando almeno
+    - sesso
+    - età
+    - provenienza (almeno a livello di provincia, si è invece liberi di indicare o meno anche il paese/la città)
+    Ti invitiamo a leggere attentamente il regolamento del server, reperibile sul canale regole.
+    Per chiarimenti rivolgiti pure ai moderatori.
+    Buona permanenza!"""
+    channel = await member.create_dm()
+    await channel.send(greetings)
 
 client.run(TOKEN)
