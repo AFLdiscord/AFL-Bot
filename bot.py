bd00bc68b6f04e50c25a5dc101204f5791ebdf5f
Skylake-dev <77413867+Skylake-dev@users.noreply.github.com>
2021-02-25 12:29:52 +0100

    Aggiunto il conteggio dei messaggi come proposto nella issue #2 e check periodico sulla scadenza del ruolo

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 # bot.py
 import os
 import json
+import asyncio
 
 import discord
+from discord.ext import tasks
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 
@@ -14,9 +16,37 @@ MAIN_CHANNEL = int(os.getenv('MAIN_CHANNEL'))
 ACTIVE_THRESHOLD = int(os.getenv('ACTIVE_THRESHOLD'))
 ACTIVE_DURATION = int(os.getenv('ACTIVE_DURATION'))
 ACTIVE_ROLE_ID = int(os.getenv('ACTIVE_ROLE'))
+GUILD_ID = int(os.getenv('GUILD_ID'))
 
 #todo caricare da file la lista delle parole bannate all'avvio
-banned_words = ['porco dio']  
+banned_words = [
+    'porco dio'
+]
+weekdays = {
+    0: "mon",
+    1: "tue",
+    2: "wed",
+    3: "thu",
+    4: "fri",
+    5: "sat",
+    6: "sun"
+}
+
+#canali di cui tenere conto dei messaggi
+active_channels_id = [
+    MAIN_CHANNEL     #solo per finalità di testing, poi anche questo verrà caricato da file di config
+]
+
+#ovviamente anche questo verrà caricato da file all'avvio
+greetings = """
+Benvenuto/a sul server discord AFL! \n
+Prima di iniziare a partecipare nel server presentati nel canale richiesta di afl indicando almeno
+- sesso
+- età
+Ti invitiamo a leggere attentamente il regolamento del server, reperibile sul canale regole.
+Per chiarimenti rivolgiti pure ai moderatori.
+Buona permanenza!
+"""
 
 #per poter ricevere le notifiche sull'unione di nuovi membri
 intents = discord.Intents.default()
@@ -31,7 +61,8 @@ async def on_ready():
     print(f'{client.user} has connected to Discord! 'f'{timestamp}')
     if(MAIN_CHANNEL is not None):
         channel = client.get_channel(MAIN_CHANNEL)
-        #await channel.send('Bot avviato alle 'f'{timestamp}')
+        await channel.send('Bot avviato alle 'f'{timestamp}')
+        periodic_checks.start()
 
 @client.event
 async def on_message(message):
@@ -52,25 +83,66 @@ async def on_message(message):
         await message.delete()
         return
 
-    if update_counter(message):
-        await message.author.add_roles(message.guild.get_role(ACTIVE_ROLE_ID))
+    update_counter(message)
 
 
 @client.event
 async def on_member_join(member):
     print('nuovo membro')
-    greetings = """Benvenuto/a sul server discord AFL! \n
-    Prima di iniziare a partecipare nel server presentati nel canale richiesta di afl indicando almeno
-    - sesso
-    - età
-    Ti invitiamo a leggere attentamente il regolamento del server, reperibile sul canale regole.
-    Per chiarimenti rivolgiti pure ai moderatori.
-    Buona permanenza!"""
     channel = await member.create_dm()
     await channel.send(greetings)
 
+@tasks.loop(minutes=1)
+async def periodic_checks():
+    """Task periodica per la gestione di:
+           - assegnamento ruolo attivo
+           - rimozione ruolo attivo
+           - (TODO)rimozione strike/violazioni
+
+        L'avvio è programmato ogni giorno allo scatto della mezzanotte.
+    """
+    print('controllo conteggio messaggi')
+    try:
+        with open('aflers.json','r') as file:
+            prev_dict = json.load(file)
+    except FileNotFoundError:
+        return
+    for key in prev_dict:
+        item = prev_dict.get(key)
+        count = count_messages(item)
+        if count >= ACTIVE_THRESHOLD:
+            #nota: non serve fare distinzione tra coloro che sono già attivi e coloro che 
+            #rinnovano il ruolo perchè in entrambi i casi l'operazione da fare è la stessa
+            item["active"] = True
+            item["expiration"] = datetime.date(datetime.now() + timedelta(days=ACTIVE_DURATION)).__str__()
+            guild = client.get_guild(GUILD_ID)
+            await guild.get_member(int(key)).add_roles(guild.get_role(ACTIVE_ROLE_ID))
+            print('member ' + key + ' is active')
+            channel = client.get_channel(MAIN_CHANNEL)
+            await channel.send('membro ' + key + ' è diventato attivo')
+
+        #rimuovo i messaggi contati 7 giorni fa
+        item[weekdays.get(datetime.today().weekday())] = 0
+
+        #pausa artificiale per controllare se il ruolo è assegnato durante i test
+        print('aspetta 15 sec...')
+        await asyncio.sleep(15)
+        print('passati')
+
+        if item["active"] is True:
+            expiration = datetime.date(datetime.strptime(item["expiration"], '%Y-%m-%d'))
+            channel = client.get_channel(MAIN_CHANNEL)
+            check = (datetime.date(datetime.now()) + timedelta(days=ACTIVE_DURATION))
+            await channel.send('expire = ' + expiration.__str__() + ' check = ' + check.__str__())
+            if expiration.__eq__((datetime.date(datetime.now()) + timedelta(days=ACTIVE_DURATION))): #prova per vedere se va
+                guild = client.get_guild(GUILD_ID)
+                await guild.get_member(int(key)).remove_roles(guild.get_role(ACTIVE_ROLE_ID))
+                await channel.send('membro ' + key + ' non più attivo :(')
+                item["active"] = False
+                item["expiration"] = None
+    update_json_file(prev_dict)
+
 def update_counter(message):
-    active = False
     if  not does_it_count(message):
         return
     prev_dict = {}
@@ -85,34 +157,45 @@ def update_counter(message):
         key = str(message.author.id)
         if key in prev_dict:
             d = prev_dict.get(key)
-            print('già in lista, aggiorno contatore')
-            d["text_count"] += 1
-            if d["text_count"] >= ACTIVE_THRESHOLD:
-                #nota: non serve fare distinzione tra coloro che sono già attivi e coloro che 
-                #rinnovano il ruolo perchè in entrambi i casi l'operazione da fare è la stessa
-                d["text_count"] = 0
-                d["active"] = True
-                d["expiration"] = datetime.date(datetime.now() + timedelta(days=7)).__str__()
-                active = True
-            update_json_file(prev_dict)
-            return active
-        print('nuovo utente, creo nuova entry')
-        afler = {
-            "ID": message.author.id,
-            "text_count": 1,
-            "violations_count": 0,
-            "active": False,
-            "expiration": None
-        }
-        prev_dict[afler.get("ID")] = afler
+            #incrementa solo il campo corrispondente al giorno corrente
+            d[weekdays.get(datetime.today().weekday())] += 1
+        else:
+            #contatore per ogni giorno per ovviare i problemi discussi nella issue #2
+            afler = {
+                "mon": 0,
+                "tue": 0,
+                "wed": 0,
+                "thu": 0,
+                "fri": 0,
+                "sat": 0,
+                "sun": 0,
+                "violations_count": 0,
+                "start_of_violation_count": None,
+                "active": False,
+                "expiration": None
+            }
+            afler[weekdays.get(datetime.today().weekday())] = 1
+            prev_dict[message.author.id] = afler
         update_json_file(prev_dict)
-        return active
 
 def does_it_count(message):
-    return True   #to do, contare solo i messaggi del server AFL sui canali specificati nel regolamento
+    if message.guild.id == GUILD_ID:
+        if message.channel.id in active_channels_id:
+            print('counts')
+            return True
+    print('doesn\'t count')
+    return False
+    
+    return True
 
 def update_json_file(data):
     with open('aflers.json', 'w') as file:
         json.dump(data, file, indent=4)
+
+def count_messages(item):
+    count = 0
+    for i in weekdays:
+        count += item[weekdays.get(i)]
+    return count
 
 client.run(TOKEN)
