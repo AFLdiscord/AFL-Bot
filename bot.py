2754590f117c1101938f2df17284e599a8a07f7c
Skylake-dev <77413867+Skylake-dev@users.noreply.github.com>
2021-03-07 21:08:35 +0100

    aggiungi comandi di moderazione: warn/unwarn/ban e centralizzato il controllo dei permessi sui comandi

--- bot.py
+++ bot.py
@@ -72,6 +72,7 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    """azioni da eseguire ad ogni messaggio"""
     if message.author == bot.user or message.author.bot:  #non conta sé stesso e gli altri bot
         return
 
@@ -85,14 +86,10 @@ async def on_message(message):
         response = 'nice'
         await message.channel.send(response)
         return
-
-    if message.content == 'greta':
-        await message.channel.send(greetings)
-        return
     
     if contains_banned_words(message.content) and message.channel.id not in EXCEPTIONAL_CHANNELS_ID:
         await message.delete()
-        await add_warn(message.author, "linguaggio inappropriato")
+        await add_warn(message.author, 'linguaggio inappropriato', 1)
         return
 
     if message.author.id not in MODERATION_ROLES_ID:
@@ -100,13 +97,14 @@ async def on_message(message):
 
     #istruzione necessaria per processare i messaggi come comandi.
     await bot.process_commands(message)
-    #escludere i comandi da update_counter?
 
 @bot.event
 async def on_message_delete(message):
     """In caso di rimozione dei messaggi va decrementato il contatore della persona che
     lo aveva scritto per evitare che messaggi non adatti vengano conteggiati nell'assegnamento del ruolo.
     """
+    if message.author == bot.user or message.author.bot:
+        return
     try:
        with open('aflers.json','r') as file:
             prev_dict = json.load(file)
@@ -135,7 +133,7 @@ async def on_reaction_add(reaction, user):
 @bot.event
 async def on_message_edit(before, after):
     """"Controlla che i messaggi non vengano editati per inserire parole della lista banned_words"""
-    if (contains_banned_words(after)):
+    if (contains_banned_words(after.content)):
         await after.delete()
 
 @bot.event
@@ -151,19 +149,6 @@ async def on_member_join(member):
     if contains_banned_words(member.display_name):
         await member.kick(reason="ForbiddenUsername")
         await channel.send(f'Il tuo username non è consentito, ritenta l\'accesso dopo averlo modificato')
-
-@bot.event
-async def on_member_remove(member):
-    """rimuove l'utente da aflers.json"""
-    if member.bot:
-        return
-    with open('aflers.json','w+') as file:
-        prev_dict = json.load(file)
-        try:
-            del prev_dict[str(member.id)]
-            json.dump(data, file, indent=4)
-        except KeyError:
-            return
 
 @bot.event
 async def on_member_update(before, after):
@@ -186,49 +171,127 @@ async def on_user_update(before, after):
         print('cambio nickname')
         await guild.get_member(after.id).edit(nick=before.display_name)
 
+async def is_mod(ctx):
+    """check sui comandi per bloccare l'utilizzo dei comandi di moderazione"""
+    return ctx.author.top_role.id in MODERATION_ROLES_ID
+
 @bot.command()
+@commands.check(is_mod)
 async def blackadd(ctx, ban_word):
     """Aggiunge stringhe alla lista contenuta in banned_words.json.
     Se la parola è composta da più parole separate da uno spazio, va messa tra ""
     """
-    if ctx.author.top_role.id not in MODERATION_ROLES_ID:
-        return
     if ban_word in banned_words:
         await ctx.send(f'la parola è già contenuta nell\'elenco')
         return
     banned_words.append(ban_word)
     update_json_file(banned_words, 'banned_words.json')
-    await ctx.send(f'parola aggiunta correttamente')
+    await ctx.send(f'parola aggiunta correttamente', delete_after=5)
 
 @bot.command()
+@commands.check(is_mod)
 async def blackremove(ctx, ban_word):
     """elimina una banned_word dall'elenco"""
-    if ctx.author.top_role.id not in MODERATION_ROLES_ID:
-        return
     if ban_word in banned_words:
         banned_words.remove(ban_word)
         update_json_file(banned_words, 'banned_words.json')
-        await ctx.send(f'la parola è stata rimossa')
+        await ctx.send(f'la parola è stata rimossa', delete_after=5)
     else:
-        await ctx.send(f'la parola non è presente nell\'elenco')
+        await ctx.send(f'la parola non è presente nell\'elenco', delete_after=5)
 
 @bot.command()
+@commands.check(is_mod)
 async def setprefix(ctx, prefix):
     """imposta prefix come nuovo prefisso del bot"""
-    if ctx.author.top_role.id not in MODERATION_ROLES_ID:
-        return
     bot.command_prefix = prefix
     os.putenv('CURRENT_PREFIX', prefix)
     await ctx.send(f'Prefisso cambiato in ``{prefix}``')
 
 @bot.command()
-async def warn(ctx, member: discord.Member, reason=None):
+@commands.check(is_mod)
+async def warn(ctx, member: discord.Member, *, reason='un moderatore ha ritenuto inopportuno il tuo comportamento'):
     """aggiunge un warn all'utente menzionato nel messaggio"""
-    if ctx.author.top_role.id not in MODERATION_ROLES_ID:
+    if member.bot:
         return
-    await add_warn(member, reason)
+    await add_warn(member, reason, 1)
+    user = '<@!' + str(member.id) + '>'
+    await ctx.send(user + ' warnato. Motivo: ' + reason)
+    await ctx.message.delete(delay=5)
 
-@tasks.loop(minutes=1)
+@bot.command()
+@commands.check(is_mod)
+async def unwarn(ctx, member: discord.Member):
+    """rimuove un warn all'utente menzionato"""
+    if member.bot:
+        return
+    reason = 'buona condotta'
+    await add_warn(member, reason, -1)
+    user = '<@!' + str(member.id) + '>'
+    await ctx.send(user + ' rimosso un warn.')
+    await ctx.message.delete(delay=5)
+
+@bot.command(aliases=['warnc', 'wc'])
+async def warncount(ctx, member:discord.Member = None):
+    """stampa nel canale in cui viene chiamato l'elenco di tutti i warn degli utenti. può accettare come parametro un 
+    username (tramite menzione) e in tal caso stampa i warn riguardanti quel membro. Può essere chiamata da chiunque"""
+    try:
+        with open('aflers.json','r') as file:
+            prev_dict = json.load(file)
+    except FileNotFoundError:
+        await ctx.send('nessuna attività registrata', delete_after=5)
+        await ctx.message.delete(delay=5)
+        return
+    warnc = []
+    if member is None:
+        for user in prev_dict:
+            mention = '<@!' + user + '>'
+            item = prev_dict.get(user)
+            count = str(item["violations_count"])
+            msg = mention + ': ' + count + ' warn'
+            warnc.append(msg)
+    else:
+        try:
+            user = prev_dict.get(str(member.id))
+            if user is None:
+               await ctx.send('nessuna attività registrata', delete_after=5)
+               await ctx.message.delete(delay=5)
+               return 
+        except KeyError:
+            await ctx.send('nessuna attività registrata', delete_after=5)
+            await ctx.message.delete(delay=5)
+            return
+        mention = '<@!' + str(member.id) + '>'
+        count = str(user["violations_count"])
+        warnc.append(mention + ': ' + count + ' warn')
+    await ctx.send(warnc)
+
+@bot.command()
+@commands.check(is_mod)
+async def ban(ctx, member: discord.Member = None, *, reason='un moderatore ha ritenuto inopportuno il tuo comportamento'):
+    """banna un membro dal server"""
+    if member is None:
+        await ctx.send('specifica un membro da bannare', delete_after=5)
+        await ctx.message.delete(delay=5)
+        return
+    user = '<@!' + str(member.id) + '>'
+    await ctx.send(user + ' bannato. Motivo: ' + reason)
+    await ctx.message.delete(delay=5)
+    penalty = 'bannato dal server.' 
+    channel = await member.create_dm()
+    await channel.send('Sei stato ' + penalty + ' Motivo: ' + reason + '.')
+    await member.ban(delete_message_days = 0, reason = reason)
+
+@bot.event
+async def on_command_error(ctx, error):
+    """generica gestione errori per evitare crash banali, da espandare in futuro"""
+    if isinstance(error, commands.CommandNotFound):
+       print('comando non trovato (se hai prefisso < ogni menzione a inizio messaggio da questo errore)')
+    elif isinstance(error, commands.CheckFailure):
+        await ctx.send('non hai i permessi per usare questo comando', delete_after=5)
+        await ctx.message.delete(delay=5)
+    print(error)
+
+@tasks.loop(hours=24)
 async def periodic_checks():
     """Task periodica per la gestione di:
            - assegnamento ruolo attivo
@@ -271,16 +334,10 @@ async def periodic_checks():
         #rimuovo i messaggi contati 7 giorni fa
         item[weekdays.get(datetime.today().weekday())] = 0
 
-        #pausa artificiale per controllare se il ruolo è assegnato durante i test
-        print('aspetta 15 sec...')
-        await asyncio.sleep(15)
-        print('passati')
-
         if item["active"] is True:
             expiration = datetime.date(datetime.strptime(item["expiration"], '%Y-%m-%d'))
             channel = bot.get_channel(MAIN_CHANNEL_ID)
             check = (datetime.date(datetime.now()) + timedelta(days=ACTIVE_DURATION))
-            await channel.send('expire = ' + expiration.__str__() + ' check = ' + check.__str__())
             if expiration.__eq__((datetime.date(datetime.now()) + timedelta(days=ACTIVE_DURATION))): #prova per vedere se va
                 guild = bot.get_guild(GUILD_ID)
                 await guild.get_member(int(key)).remove_roles(guild.get_role(ACTIVE_ROLE_ID))
@@ -333,7 +390,6 @@ def does_it_count(message):
     if message.guild is not None:
         if message.guild.id == GUILD_ID:
             if message.channel.id in ACTIVE_CHANNELS_ID:
-                print('counts')
                 return True
         print('doesn\'t count')
     return False
@@ -362,16 +418,16 @@ def contains_banned_words(text):
     text_to_check = re.sub("3", "e", text_to_check)
     text_to_check = re.sub("7", "t", text_to_check)
     for word in banned_words:
-        regex_word = "+ *\W*".join(word)
+        regex_word = '+ *\W*'.join(word)
         x = re.search(regex_word, text_to_check)
         if x is not None:
             return True
     return False
 
-async def add_warn(member, reason):
-    """incrementa il numero di violazioni e tiene traccia della prima violazione commesa"""
+async def add_warn(member, reason, number):
+    """incrementa o decremente il numero di violazioni di numero e tiene traccia dell'ultima violazione commesa"""
     prev_dict = {}
-    penalty = "warnato."
+    penalty = 'warnato.'
     try:
         with open('aflers.json','r') as file:
             prev_dict = json.load(file)
@@ -383,17 +439,31 @@ async def add_warn(member, reason):
         key = str(member.id)
         if key in prev_dict:
             d = prev_dict.get(key)
-            if d["violations_count"] == 2:
-                await member.add_roles(bot.get_guild(GUILD_ID).get_role(UNDER_SURVEILLANCE_ID))
-                penalty = "sottoposto a sorveglianza, il prossimo sara' un ban."
-            elif d["violations_count"] == 3:
-                k = reason
-                await member.ban(delete_message_days = 0, reason = k)
-                penalty = "bannato dal server."
+            d["violations_count"] += number
             d["last_violation_count"] = datetime.date(datetime.now()).__str__()
-            d["violations_count"] += 1
+            if d["violations_count"] < 0:
+                d["violations_count"] = 0
+                d["last_violation_count"] = None
+                return
+            if number < 0:  #non deve controllare se è un unwarn
+                return
+            if d["violations_count"] == 3:
+                await member.add_roles(bot.get_guild(GUILD_ID).get_role(UNDER_SURVEILLANCE_ID))
+                penalty = 'sottoposto a sorveglianza, il prossimo sara\' un ban.'
+                channel = await member.create_dm()
+                await channel.send('Sei stato ' + penalty + ' Motivo: ' + reason + '.')
+            elif d["violations_count"] == 4:
+                penalty = 'bannato dal server.' 
+                channel = await member.create_dm()
+                await channel.send('Sei stato ' + penalty + ' Motivo: ' + reason + '.')
+                await member.ban(delete_message_days = 0, reason = reason)   
+            else:
+                channel = await member.create_dm()
+                await channel.send('Sei stato ' + penalty + ' Motivo: ' + reason + '.')
         else:
             #contatore per ogni giorno per ovviare i problemi discussi nella issue #2
+            if number < 0:
+                return
             afler = {
                 "mon": 0,
                 "tue": 0,
@@ -402,14 +472,12 @@ async def add_warn(member, reason):
                 "fri": 0,
                 "sat": 0,
                 "sun": 0,
-                "violations_count": 1,
+                "violations_count": number,
                 "last_violation_count": datetime.date(datetime.now()).__str__(),
                 "active": False,
                 "expiration": None
             }
             prev_dict[key] = afler
-        channel = await member.create_dm()
-        await channel.send("Sei stato " + penalty + " Motivo: " + reason + ".")
         update_json_file(prev_dict, 'aflers.json')
 
 bot.run(TOKEN)
