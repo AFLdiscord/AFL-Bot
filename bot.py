7bfe25dc3b5afd90b22003d1eab34ec5c7186a4d
Skylake-dev <77413867+Skylake-dev@users.noreply.github.com>
2021-03-31 12:21:07 +0200

    spostata on_ready e task nella cog degli eventi

--- bot.py
+++ bot.py
@@ -4,16 +4,12 @@ import json
 import asyncio
 import re
 import logging
-
 import discord
-from discord.ext import tasks
 from discord.ext import commands
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from cogs import sharedFunctions
 from cogs.sharedFunctions import BannedWords, Config
-
-__version__ = 'v0.4'
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
@@ -71,73 +67,5 @@ async def reload(ctx, *args):
             await ctx.message.delete(delay=5)
     if reloaded.__len__ != 0:
         await ctx.send('Estensioni ' + reloaded + 'ricaricate correttamente.')
-
-@bot.event
-async def on_ready():
-    timestamp = datetime.time(datetime.now())
-    botstat = discord.Game(name='AFL ' + __version__)
-    await bot.change_presence(activity=botstat)
-    print(f'{bot.user} has connected to Discord! 'f'{timestamp}')
-    if(Config.config['main_channel_id'] is not None):
-        channel = bot.get_channel(Config.config['main_channel_id'])
-        await channel.send('AFL Bot `' + __version__ + '` avviato alle `'f'{timestamp}`. Il prefisso è: `{bot.command_prefix}`')
-    if not periodic_checks.is_running():    #per evitare RuntimeExceptions se il bot si disconnette per un periodo prolungato
-        print('avvio task')
-        periodic_checks.start()
-    else:
-        print('task già avviata')
-
-@tasks.loop(hours=24)
-async def periodic_checks():
-    """Task periodica per la gestione di:
-           - assegnamento ruolo attivo (i mod sono esclusi)
-           - rimozione ruolo attivo
-           - rimozione strike/violazioni
-
-        L'avvio è programmato ogni giorno allo scatto della mezzanotte.
-    """
-    print('controllo conteggio messaggi')
-    try:
-        with open('aflers.json','r') as file:
-            prev_dict = json.load(file)
-    except FileNotFoundError:
-        return
-    for key in prev_dict:
-        item = prev_dict[key]
-        sharedFunctions.clean(item)
-        count = sharedFunctions.count_consolidated_messages(item)
-        if count >= Config.config['active_threshold'] and bot.get_guild(Config.config['guild_id']).get_member(int(key)).top_role.id not in Config.config['moderation_roles_id']:
-            item["active"] = True
-            item["expiration"] = datetime.date(datetime.now() + timedelta(days=Config.config['active_duration'])).__str__()
-            guild = bot.get_guild(Config.config['guild_id'])
-            await guild.get_member(int(key)).add_roles(guild.get_role(Config.config['active_role_id']))
-            print('member ' + key + ' is active')
-            channel = bot.get_channel(Config.config['main_channel_id'])
-            await channel.send('membro <@!' + key + '> è diventato attivo')
-            #azzero tutti i contatori
-            for i in sharedFunctions.weekdays:
-                item[sharedFunctions.weekdays.get(i)] = 0
-
-        #controllo sulla data dell'ultima violazione, ed eventuale reset
-        if item["last_violation_count"] is not None:
-            expiration = datetime.date(datetime.strptime(item["last_violation_count"], '%Y-%m-%d'))
-            if (expiration + timedelta(days=Config.config["violations_reset_days"])).__eq__(datetime.date(datetime.now())):
-                print('reset violazioni di ' + key)
-                item["violations_count"] = 0
-                item["last_violation_count"] = None
-
-        #rimuovo i messaggi contati 7 giorni fa
-        item[sharedFunctions.weekdays.get(datetime.today().weekday())] = 0
-
-        if item["active"] is True:
-            expiration = datetime.date(datetime.strptime(item["expiration"], '%Y-%m-%d'))
-            channel = bot.get_channel(Config.config['main_channel_id'])
-            if expiration.__eq__((datetime.date(datetime.now()))):
-                guild = bot.get_guild(Config.config['guild_id'])
-                await guild.get_member(int(key)).remove_roles(guild.get_role(Config.config['active_role_id']))
-                await channel.send('membro ' + key + ' non più attivo :(')
-                item["active"] = False
-                item["expiration"] = None
-    sharedFunctions.update_json_file(prev_dict, 'aflers.json')
 
 bot.run(TOKEN)
--- cogs/EventsCog.py
+++ cogs/EventsCog.py
@@ -1,6 +1,6 @@
 import json
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from datetime import datetime, timedelta
 from cogs import sharedFunctions
 from cogs.sharedFunctions import BannedWords, Config
@@ -11,6 +11,7 @@ class EventCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.__version__ = 'v0.4'
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -169,6 +170,74 @@ class EventCog(commands.Cog):
             await ctx.send('non hai i permessi per usare questo comando', delete_after=5)
             await ctx.message.delete(delay=5)
         print(error)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        timestamp = datetime.time(datetime.now())
+        botstat = discord.Game(name='AFL ' + self.__version__)
+        await self.bot.change_presence(activity=botstat)
+        print(f'{self.bot.user} has connected to Discord! 'f'{timestamp}')
+        if not self.periodic_checks.is_running():    #per evitare RuntimeExceptions se il bot si disconnette per un periodo prolungato
+            if(Config.config['main_channel_id'] is not None):
+                channel = self.bot.get_channel(Config.config['main_channel_id'])
+                await channel.send('AFL Bot `' + self.__version__ + '` avviato alle `'f'{timestamp}`. Il prefisso è: `{self.bot.command_prefix}`')
+            print('avvio task')
+            self.periodic_checks.start()
+        else:
+            print('task già avviata')
+
+    @tasks.loop(hours=24)
+    async def periodic_checks(self):
+        """Task periodica per la gestione di:
+            - assegnamento ruolo attivo (i mod sono esclusi)
+            - rimozione ruolo attivo
+            - rimozione strike/violazioni
+
+            L'avvio è programmato ogni giorno allo scatto della mezzanotte.
+        """
+        print('controllo conteggio messaggi')
+        try:
+            with open('aflers.json','r') as file:
+                prev_dict = json.load(file)
+        except FileNotFoundError:
+            return
+        for key in prev_dict:
+            item = prev_dict[key]
+            sharedFunctions.clean(item)
+            count = sharedFunctions.count_consolidated_messages(item)
+            if count >= Config.config['active_threshold'] and self.bot.get_guild(Config.config['guild_id']).get_member(int(key)).top_role.id not in Config.config['moderation_roles_id']:
+                item["active"] = True
+                item["expiration"] = datetime.date(datetime.now() + timedelta(days=Config.config['active_duration'])).__str__()
+                guild = self.bot.get_guild(Config.config['guild_id'])
+                await guild.get_member(int(key)).add_roles(guild.get_role(Config.config['active_role_id']))
+                print('member ' + key + ' is active')
+                channel = self.bot.get_channel(Config.config['main_channel_id'])
+                await channel.send('membro <@!' + key + '> è diventato attivo')
+                #azzero tutti i contatori
+                for i in sharedFunctions.weekdays:
+                    item[sharedFunctions.weekdays.get(i)] = 0
+
+            #controllo sulla data dell'ultima violazione, ed eventuale reset
+            if item["last_violation_count"] is not None:
+                expiration = datetime.date(datetime.strptime(item["last_violation_count"], '%Y-%m-%d'))
+                if (expiration + timedelta(days=Config.config["violations_reset_days"])).__eq__(datetime.date(datetime.now())):
+                    print('reset violazioni di ' + key)
+                    item["violations_count"] = 0
+                    item["last_violation_count"] = None
+
+            #rimuovo i messaggi contati 7 giorni fa
+            item[sharedFunctions.weekdays.get(datetime.today().weekday())] = 0
+
+            if item["active"] is True:
+                expiration = datetime.date(datetime.strptime(item["expiration"], '%Y-%m-%d'))
+                channel = self.bot.get_channel(Config.config['main_channel_id'])
+                if expiration.__eq__((datetime.date(datetime.now()))):
+                    guild = self.bot.get_guild(Config.config['guild_id'])
+                    await guild.get_member(int(key)).remove_roles(guild.get_role(Config.config['active_role_id']))
+                    await channel.send('membro ' + key + ' non più attivo :(')
+                    item["active"] = False
+                    item["expiration"] = None
+        sharedFunctions.update_json_file(prev_dict, 'aflers.json')
 
 def update_counter(cog, message):
     """aggiorna il contatore dell'utente che ha mandato il messaggio. Se l'utente non era presente lo aggiunge
