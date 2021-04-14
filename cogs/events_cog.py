98a90f2ce98868593767095e68654c0b6428b8fc
Skylake-dev <77413867+Skylake-dev@users.noreply.github.com>
2021-04-14 11:52:05 +0200

    bump versione a 0.5

--- cogs/events_cog.py
+++ cogs/events_cog.py
@@ -39,7 +39,7 @@ class EventCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.__version__ = 'v0.4.2'
+        self.__version__ = 'v0.5'
 
     @commands.Cog.listener()
     async def on_message(self, message):
