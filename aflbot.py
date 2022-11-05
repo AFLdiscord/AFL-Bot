963e0e9069e73099c8200132721a0e3d613074dd
cathartyc <67878876+cathartyc@users.noreply.github.com>
2022-11-05 01:10:11 +0100

    rimosso codice inutile

--- aflbot.py
+++ aflbot.py
@@ -1,20 +1,17 @@
 from __future__ import annotations
 from datetime import datetime
-from typing import Any, ClassVar, Optional
+from typing import Any, Optional
 
 import discord
-from discord.utils import MISSING
 from discord.ext import commands
 from utils.shared_functions import get_extensions
 
 class AFLBot(commands.Bot):
-    """Istanza del bot. È un singleton e rispetto a commands.Bot ha le
-    seguenti informazioni aggiuntive:
+    """Istanza del bot. Rispetto a commands.Bot ha le seguenti
+    informazioni aggiuntive:
     - version: str           tiene traccia della versione
     - start_time: datetime   timestamp di avvio del bot
     """
-    _bot: ClassVar[AFLBot] = MISSING
-
     def __init__(
         self,
         command_prefix: str,
@@ -23,8 +20,6 @@ class AFLBot(commands.Bot):
         tree_cls=discord.app_commands.CommandTree,
         description: Optional[str] = None,
         **options: Any) -> None:
-        if AFLBot._bot is not MISSING:
-            raise RuntimeError('Bot già istanziato, usa get_instance')
         super().__init__(
             command_prefix,
             tree_cls=tree_cls,
@@ -34,10 +29,6 @@ class AFLBot(commands.Bot):
             **options)
         self.version: str
         self.start_time: datetime
-
-    @classmethod
-    def get_instance(cls) -> AFLBot:
-        return cls._bot
 
     # carico i moduli dei comandi
     async def setup_hook(self) -> None:
