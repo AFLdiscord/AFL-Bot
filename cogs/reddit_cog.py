98abcec47c8b037229e50c41185ffe947cd62263
cathartyc <67878876+cathartyc@users.noreply.github.com>
2023-03-01 11:26:49 +0100

    esclusi i posti eliminati dai post dei subreddit

--- cogs/reddit_cog.py
+++ cogs/reddit_cog.py
@@ -178,7 +178,9 @@ class RedditCog(commands.Cog):
                 await self.create_post_iterator(sub)
                 submission = await generator.__anext__()
             if (not submission.stickied and
-                submission.domain != 'v.redd.it'):
+                submission.domain != 'v.redd.it' and
+                submission.banned_by is None and
+                submission.author is not None):
                 return submission
 
 
