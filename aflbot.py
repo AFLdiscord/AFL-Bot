aaffa0f4c5f0e54183b3332f0fa2d01b91130444
cathartyc <67878876+cathartyc@users.noreply.github.com>
2026-01-30 16:32:55 +0100

    pulizia init di aflbot

--- aflbot.py
+++ aflbot.py
@@ -1,32 +1,17 @@
-from __future__ import annotations
 from datetime import datetime
-from typing import Any, Optional
 
-import discord
 from discord.ext import commands
 from utils.shared_functions import get_extensions
 
 class AFLBot(commands.Bot):
     """Istanza del bot. Rispetto a commands.Bot ha le seguenti
     informazioni aggiuntive:
+
     - version: str           tiene traccia della versione
     - start_time: datetime   timestamp di avvio del bot
     """
-    def __init__(
-        self,
-        command_prefix: str,
-        intents: discord.Intents,
-        help_command: commands.HelpCommand = commands.DefaultHelpCommand(),
-        tree_cls=discord.app_commands.CommandTree,
-        description: Optional[str] = None,
-        **options: Any) -> None:
-        super().__init__(
-            command_prefix,
-            tree_cls=tree_cls,
-            help_command=help_command,
-            description=description,
-            intents=intents,
-            **options)
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.version: str
         self.start_time: datetime
 
